2a87be5a0438863931384f635e7da52fcde49576
crudd12 <crudd12@gmail.com>
2024-01-17 13:54:20 -0800

    updated logic to allow the character to appear as if its walking

--- move.js
+++ move.js
@@ -1,12 +1,60 @@
 function move(element) {
-    element.style.position = 'fixed'
+  element.style.position = "fixed";
 
-    function moveToCoordinates(left, bottom) {
-        element.style.left = left + 'px'
-        element.style.bottom = bottom + 'px'
+  function moveToCoordinates(left, bottom) {
+    element.style.left = left + "px";
+    element.style.bottom = bottom + "px";
+  }
+
+  function moveWithArrowKeys(left, bottom, handleDirectionChange) {
+    let direction = null;
+    let x = left;
+    let y = bottom;
+
+    function moveCharacter() {
+      if (direction === "west") {
+        x = x - 1;
+      }
+      if (direction === "north") {
+        y = y + 1;
+      }
+      if (direction === "east") {
+        x = x + 1;
+      }
+      if (direction === "south") {
+        y = y - 1;
+      }
+      element.style.left = x + "px";
+      element.style.bottom = y + "px";
     }
 
-    return {
-        to: moveToCoordinates
-    }
+    setInterval(moveCharacter, 1);
+
+    document.addEventListener("keydown", function (e) {
+      if (e.repeat) return;
+      if (e.key === "ArrowLeft") {
+        direction = "west";
+      }
+      if (e.key === "ArrowUp") {
+        direction = "north";
+      }
+      if (e.key === "ArrowRight") {
+        direction = "east";
+      }
+      if (e.key === "ArrowDown") {
+        direction = "south";
+      }
+      handleDirectionChange(direction);
+    });
+
+    document.addEventListener("keyup", function (e) {
+      direction = null;
+      handleDirectionChange(direction);
+    });
+  }
+
+  return {
+    to: moveToCoordinates,
+    withArrowKeys: moveWithArrowKeys,
+  };
 }
